265e1a816dc86fbd4d5a6c5937d05cf42bb9450d
sahil <sahilkothari114@gmail.com>
2018-06-20 14:15:48 +0530

    Gson Configured

--- src/main/java/com/bookstagram/controller/Signup.java
+++ src/main/java/com/bookstagram/controller/Signup.java
@@ -9,8 +9,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
+
 import com.bookstagram.DTO.User;
 import com.bookstagram.service.SignupService;
+import com.bookstagram.util.BookstagramConstant;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * Servlet implementation class Signup
@@ -19,18 +24,23 @@ import com.bookstagram.service.SignupService;
 public class Signup extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException,SecurityException {
 		//response.setContentType("text/html");
-		User user = new User();
+/*		User user = new User();
 		user.setUserName(request.getParameter("username"));
 		user.setPassword(request.getParameter("password"));
-		user.setEmail(request.getParameter("email"));
+		user.setEmail(request.getParameter("email"));*/
+		
+		String jsonString = IOUtils.toString(request.getInputStream(),BookstagramConstant.CHARACTER_ENCODING);
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		User user = gson.fromJson(jsonString, User.class);
+		
+		
+		
 		SignupService signupService = new SignupService();
 		signupService.SignUp(user);
-		RequestDispatcher rd = request.getRequestDispatcher("index2.jsp")
-		;
+		@SuppressWarnings("unused")
+		RequestDispatcher rd = request.getRequestDispatcher("index2.jsp");
 	}
 
 }
---
+++ src/main/java/com/bookstagram/util/BookstagramConstant.java
@@ -0,0 +1,5 @@
+package com.bookstagram.util;
+
+public class BookstagramConstant {
+	public  final static String CHARACTER_ENCODING = "UTF-8";
+}
